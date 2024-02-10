b5ec78f65ae3a973a0eda4d2a32aa5cb1d4d4c3e
danwood1992 <gunnerwoody449@gmail.com>
2024-02-10 15:37:27 +0000

    Set up db Conn.

--- base.py
+++ base.py
@@ -4,6 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@trivia-db:5432/postgres'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- middlewares.py
+++ middlewares.py
@@ -1,12 +1,15 @@
 from flask_cors import CORS
 def setup_middlewares(app):
+  
 
     CORS(app)
 
     @app.after_request
     def after_request_func(response):
+      
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 
         return response
+    
