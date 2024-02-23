be5e3249ec9a81ff4179c534ce31ce6e5311a186
danwood1992 <gunnerwoody449@gmail.com>
2024-02-23 22:35:24 +0000

    block update

--- frontend/src/components/elements/QuizLink.tsx
+++ frontend/src/components/elements/QuizLink.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 interface QuizLinkProps {
     link: string;
     name: string;
@@ -6,8 +7,9 @@ interface QuizLinkProps {
 export default function QuizLink({link,name}: QuizLinkProps) {
     return (
         <div className="m-2 text-center p-2 rounded border shadow hover:opacity-1">
-            <p>{link}</p>
+            <Link href={link}>
             <p>{name}</p>
+            </Link>
         </div>
     )
 }
