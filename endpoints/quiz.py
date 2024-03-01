cfd3827d43dd1576efbc84c426b0b9883dfb3019
danwood1992 <gunnerwoody449@gmail.com>
2024-03-01 09:05:38 +0000

    Ad endpoint to delete session

--- endpoints/quiz.py
+++ endpoints/quiz.py
@@ -1,7 +1,8 @@
 from flask import jsonify
-from models import Question, Quiz, QuizSession
+from models import Question, Quiz, QuizSession, Category, QuizQuestion
 from base import app
 from datetime import datetime
+from flask import request
 
 @app.route('/quiz/<uuid:quiz_id>/play', methods=['GET'])
 def play_quiz(quiz_id):
@@ -20,7 +21,6 @@ def play_quiz(quiz_id):
         })
 
 
-    
 
 @app.route('/quizzes', methods=['GET'])
 def get_quizzes():
@@ -49,4 +49,55 @@ def delete_quiz(quiz_id):
         return jsonify({
             'success': True,
             'message': 'Quiz deleted'
+        })
+
+
+@app.route('/quiz/<uuid:quiz_id>/questions', methods=['GET'])
+def get_questions(quiz_id):
+    questions = Question.query.filter_by(quiz_id=quiz_id).all()
+    if len(questions) == 0:
+        return jsonify({
+            'success': False,
+            'message': 'No questions found'
+        })
+    else:
+        return jsonify({
+            'success': True,
+            'message': 'Questions found',
+            'questions': [question.format() for question in questions]
+        })
+
+@app.route('/quiz/<uuid:quiz_id>/question', methods=['POST'])
+def add_question(quiz_id):
+    data = request.get_json()
+    return jsonify({
+        'success': True,
+        'message': 'Question added',
+        
+    })
+    
+@app.route('/categories', methods=['POST'])
+def add_category():
+    data = request.get_json()
+    new_category = Category(name=data['name'])
+    new_category.add()
+    return jsonify({
+        'success': True,
+        'message': 'Category added',
+        'category': new_category.format()
+    })
+    
+@app.route('/questions/category/<uuid:category_id>', methods=['POST'])
+def get_questions_by_category(category_id):
+    questions = Question.query.filter_by(category_id=category_id).all()
+    if len(questions) == 0:
+        return jsonify({
+            'success': False,
+            'message': 'No questions found'
+        })
+    else:
+        return jsonify({
+            'success': True,
+            'message': 'Questions found',
+            'questions': [question.format() for question in questions]
         })
