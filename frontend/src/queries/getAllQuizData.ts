bfd237af1df3c88a26e848967c87a59576237c95
danwood1992 <gunnerwoody449@gmail.com>
2024-03-03 21:21:23 +0000

    update ports

--- frontend/src/queries/getAllQuizData.ts
+++ frontend/src/queries/getAllQuizData.ts
@@ -1,5 +1,7 @@
+import { api_url } from "@/utils/NetworkUtils"
+
 export default async function getAllQuizData() {
-    const response = await fetch('http://trivia-api:5125/quizzes', { next: { revalidate: 60 } })
+    const response = await fetch(`${api_url}/quizzes`, { next: { revalidate: 60 } })
     
     if (!response.ok) {
       throw new Error('Failed to fetch data')
