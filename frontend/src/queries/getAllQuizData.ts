82803231f272009fb8b987610ec47d3a52510fdc
danwood1992 <gunnerwoody449@gmail.com>
2024-03-03 21:29:27 +0000

    fix url error

--- frontend/src/queries/getAllQuizData.ts
+++ frontend/src/queries/getAllQuizData.ts
@@ -2,7 +2,7 @@ import { api_url } from "@/utils/NetworkUtils"
 
 export default async function getAllQuizData() {
     console.log("api_url", api_url)
-    const response = await fetch(`${api_url}api/quizzes`, { next: { revalidate: 60 } })
+    const response = await fetch(`${api_url}/api/quizzes`, { next: { revalidate: 60 } })
     
     if (!response.ok) {
       throw new Error('Failed to fetch data')
