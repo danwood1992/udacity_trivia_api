8b69b3d15f253f4ce24b4998b935bf04727509e8
danwood1992 <gunnerwoody449@gmail.com>
2024-03-03 21:23:27 +0000

    add console log

--- frontend/src/queries/getAllQuizData.ts
+++ frontend/src/queries/getAllQuizData.ts
@@ -1,10 +1,12 @@
 import { api_url } from "@/utils/NetworkUtils"
 
 export default async function getAllQuizData() {
+    console.log("api_url", api_url)
     const response = await fetch(`${api_url}/quizzes`, { next: { revalidate: 60 } })
     
     if (!response.ok) {
       throw new Error('Failed to fetch data')
+      
     }
    
     return response.json()
