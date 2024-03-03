1476eb562e3a612937af0f4df32026e72a5527aa
danwood1992 <gunnerwoody449@gmail.com>
2024-03-03 21:07:40 +0000

    update ports

--- frontend/src/queries/getQuizData.ts
+++ frontend/src/queries/getQuizData.ts
@@ -1,9 +1,11 @@
+import { api_url } from '../utils/NetworkUtils'
+
 interface QuizData {
     quiz_id: string;
   }
 
 export default async function getQuizData({quiz_id}: QuizData) {
-    const response = await fetch(`http://localhost:5125/api/quiz/${quiz_id}/play`, { next: { revalidate: 60 } })
+    const response = await fetch(`${api_url}/api/quiz/${quiz_id}/play`, { next: { revalidate: 60 } })
     
     if (!response.ok) {
       throw new Error('Failed to fetch data')
---
+++ frontend/src/utils/NetworkUtils.ts
@@ -0,0 +1,3 @@
+// export const api_url = "http://trivia.imperisoft:5125";
+
+export const api_url = "http://localhost:5125";
