a82235e760d54c56e240fadf69e7dcf8ae009eb4
danwood1992 <gunnerwoody449@gmail.com>
2024-03-13 21:26:46 +0000

    change ports

--- frontend/src/utils/NetworkUtils.ts
+++ frontend/src/utils/NetworkUtils.ts
@@ -1,9 +1,11 @@
 
 export function getApiUrl() {
     if (process.env.NODE_ENV === 'development') {
+        console.log("Using development API URL");
         return "http://localhost:5125";
     }
     else {
+        console.log("Using production API URL");
         return "http://trivia.imperisoft.co.uk:5125";
     }
 }
